dea1261d592dc05a8dc477bb9cc26766d74e8cd1
Victoria B <viktorija.bourassa@gmail.com>
2019-03-01 13:44:14 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,24 @@
+
+
+
+class PigLatinizer
+  #attr_accessor :words
+
+  #def initialize(words=nil)
+#    @text = words
+#  end
+
+  def piglatinize(text)
+    @array = text.split(" ")
+    new_arr = []
+    @array.each do |word|
+    new_word = word + word[0]
+    new_word[0] = ''
+    new_word = new_word + "ay"
+    new_arr << new_word
+  end
+  new_string = new_arr.join(" ")
+  new_string
+  end
+
+end
